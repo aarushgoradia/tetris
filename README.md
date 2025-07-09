b5c09ff17eab474e79bc225890ea342e0d1ad1cf
Aarush Goradia <goradia.aarush@gmail.com>
2025-07-09 13:33:33 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tetris
+
+This is a C++ implementation of Tetris. This will initially be for the terminal, then ported
+to a proper GUI using Raylib.
---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿// Tetris.cpp : Defines the entry point for the application.
+//
+
+#include "Tetris.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
